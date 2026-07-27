1ae535fc3f48bbe4eaeba0e4b9f36fcb7511fa21
jonathanjosuemoralesmurillo-hub <jonathan.josue.morales.murillo@gmail.com>
2026-07-27 00:41:04 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -4,6 +4,6 @@ using namespace std;
 // EIF-201 Programación I — Semana_01
 // Compile: g++ -std=c++11 -Wall main.cpp -o programa
 int main() {
-    cout << "EIF-201 | Semana_01 lista." << endl;
+    cout << "EIY-405 | Semana_01 lista." << endl;
     return 0;
 }
